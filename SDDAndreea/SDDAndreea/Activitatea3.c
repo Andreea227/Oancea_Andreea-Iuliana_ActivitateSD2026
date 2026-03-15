d88a35dbd485ea7e3079ead7caa2cf97006bb372
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-03-15 19:40:34 +0200

    Adăugare declarații structuri

---
+++ SDDAndreea/SDDAndreea/Activitatea3.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Restaurant Restaurant;
+typedef struct Nod Nod;
+typedef struct ListaDubla ListaDubla;
