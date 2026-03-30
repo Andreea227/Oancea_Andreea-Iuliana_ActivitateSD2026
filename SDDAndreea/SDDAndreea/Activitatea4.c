e509a3fea64c9f9bf5ec2d6f1e6d0e256ab5c57b
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-03-30 21:00:22 +0300

    Adaugare structuri cafenea

--- SDDAndreea/SDDAndreea/Activitatea4.c
+++ SDDAndreea/SDDAndreea/Activitatea4.c
@@ -5,3 +5,9 @@ typedef struct cafenea Cafenea;
 typedef struct nod Nod;
 typedef struct nodLdi nodLdi;
 typedef struct listaDubla listaDubla;
+
+struct cafenea {
+	int nrLocuri;
+	char* nume;
+	float incasari;
+};
