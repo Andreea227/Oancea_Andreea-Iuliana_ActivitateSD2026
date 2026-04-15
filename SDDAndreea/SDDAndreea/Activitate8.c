50230d1877f84ec43d05dbb3d0ba566b92a1f354
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-04-15 21:23:46 +0300

    Implementare functie initAngajat

--- SDDAndreea/SDDAndreea/Activitate8.c
+++ SDDAndreea/SDDAndreea/Activitate8.c
@@ -23,3 +23,13 @@ struct HashTable
 	int dimensiune;
 	Nod** vector;
 };
+
+Angajat initAngajat(const char* nume, int varsta, float salariu) {
+	Angajat a;
+	a.varsta = varsta;
+	a.salariu = salariu;
+	a.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+	strcpy(a.nume, nume);
+
+	return a;
+}
