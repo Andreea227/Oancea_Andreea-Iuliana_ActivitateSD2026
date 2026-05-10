960e5d716cf891425e6877bc703ca453a061ee48
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-10 19:11:59 +0300

    Implementare functie citire film din fisier

--- SDDAndreea/SDDAndreea/Activitatea15.c
+++ SDDAndreea/SDDAndreea/Activitatea15.c
@@ -19,3 +19,33 @@ struct Heap {
 	int nrElemente;
 };
 typedef struct Heap Heap;
+
+Film citireFilmDinFisier(FILE* file) {
+
+	char buffer[100];
+	char sep[3] = ",\n";
+
+	fgets(buffer, 100, file);
+
+	char* aux;
+	Film f;
+
+	aux = strtok(buffer, sep);
+	f.id = atoi(aux);
+
+	f.durata = atoi(strtok(NULL, sep));
+
+	f.rating = atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	f.titlu = malloc(strlen(aux) + 1);
+	strcpy_s(f.titlu, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	f.regizor = malloc(strlen(aux) + 1);
+	strcpy_s(f.regizor, strlen(aux) + 1, aux);
+
+	f.categorie = *strtok(NULL, sep);
+
+	return f;
+}
