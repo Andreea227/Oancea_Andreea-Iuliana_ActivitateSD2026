856d2738d2dbfa40fbd235273d11da20c62a08cc
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-10 17:45:30 +0300

    Implementare functie citire produs din fisier

--- SDDAndreea/SDDAndreea/Activitatea12.c
+++ SDDAndreea/SDDAndreea/Activitatea12.c
@@ -19,3 +19,33 @@ struct Heap {
 	int nrElemente;
 };
 typedef struct Heap Heap;
+
+Produs citireProdusDinFisier(FILE* file) {
+
+	char buffer[100];
+	char sep[3] = ",\n";
+
+	fgets(buffer, 100, file);
+
+	char* aux;
+	Produs p;
+
+	aux = strtok(buffer, sep);
+	p.id = atoi(aux);
+
+	p.stoc = atoi(strtok(NULL, sep));
+
+	p.pret = atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	p.denumire = malloc(strlen(aux) + 1);
+	strcpy_s(p.denumire, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	p.producator = malloc(strlen(aux) + 1);
+	strcpy_s(p.producator, strlen(aux) + 1, aux);
+
+	p.categorie = *strtok(NULL, sep);
+
+	return p;
+}
