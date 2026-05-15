5a48c2c77abc9ca8ffbf697b2bae86a609e12348
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-15 14:13:15 +0300

    Definire structuri Student si Heap

---
+++ SDDAndreea/SDDAndreea/Activitatea20.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct StructuraStudent {
+	int id;
+	int varsta;
+	float medie;
+	char* nume;
+	char* facultate;
+	unsigned char an;
+};
+typedef struct StructuraStudent Student;
+
+struct Heap {
+	int lungime;
+	Student* studenti;
+	int nrElemente;
+};
+typedef struct Heap Heap;
