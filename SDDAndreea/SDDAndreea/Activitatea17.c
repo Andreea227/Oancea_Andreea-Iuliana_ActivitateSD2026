b6723c4f6f3215de62fc05bfc221d285044032de
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-15 13:49:46 +0300

    Citire cont din fisier

--- SDDAndreea/SDDAndreea/Activitatea17.c
+++ SDDAndreea/SDDAndreea/Activitatea17.c
@@ -20,3 +20,31 @@ struct Heap {
 };
 typedef struct Heap Heap;
 
+ContBancar citireContDinFisier(FILE* file) {
+
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+
+	char* aux;
+	ContBancar c;
+
+	aux = strtok(buffer, sep);
+	c.id = atoi(aux);
+
+	c.sold = atoi(strtok(NULL, sep));
+
+	c.dobanda = atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	c.titular = malloc(strlen(aux) + 1);
+	strcpy_s(c.titular, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	c.banca = malloc(strlen(aux) + 1);
+	strcpy_s(c.banca, strlen(aux) + 1, aux);
+
+	c.tip = *strtok(NULL, sep);
+
+	return c;
+}
