a7a3523b299f209d455e61a1af8ca04882598b57
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-15 14:06:02 +0300

    Citire telefon din fisier

--- SDDAndreea/SDDAndreea/Activitatea19.c
+++ SDDAndreea/SDDAndreea/Activitatea19.c
@@ -19,3 +19,32 @@ struct Heap {
 	int nrElemente;
 };
 typedef struct Heap Heap;
+
+Telefon citireTelefonDinFisier(FILE* file) {
+
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+
+	char* aux;
+	Telefon t;
+
+	aux = strtok(buffer, sep);
+	t.id = atoi(aux);
+
+	t.stoc = atoi(strtok(NULL, sep));
+
+	t.pret = atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	t.brand = malloc(strlen(aux) + 1);
+	strcpy_s(t.brand, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	t.model = malloc(strlen(aux) + 1);
+	strcpy_s(t.model, strlen(aux) + 1, aux);
+
+	t.memorie = *strtok(NULL, sep);
+
+	return t;
+}
