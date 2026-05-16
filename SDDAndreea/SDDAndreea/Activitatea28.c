5b9db177ffeabbf0b1433eb1c91635a77d545184
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-16 19:40:31 +0300

    Citire prajitura din fisier

--- SDDAndreea/SDDAndreea/Activitatea28.c
+++ SDDAndreea/SDDAndreea/Activitatea28.c
@@ -19,3 +19,30 @@ struct Heap {
 	int nrElemente;
 };
 typedef struct Heap Heap;
+Prajitura citirePrajituraDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+
+	char* aux;
+	Prajitura p;
+
+	aux = strtok(buffer, sep);
+	p.id = atoi(aux);
+
+	p.calorii = atoi(strtok(NULL, sep));
+
+	p.pret = atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	p.nume = malloc(strlen(aux) + 1);
+	strcpy(p.nume, aux);
+
+	aux = strtok(NULL, sep);
+	p.cofetarie = malloc(strlen(aux) + 1);
+	strcpy(p.cofetarie, aux);
+
+	p.tip = *strtok(NULL, sep);
+
+	return p;
+}
