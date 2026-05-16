689ef744fcff2cbae327158a86be9f9e933d6bc0
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-16 20:29:47 +0300

    dezechilibru stanga

--- SDDAndreea/SDDAndreea/Activitatea32.c
+++ SDDAndreea/SDDAndreea/Activitatea32.c
@@ -93,3 +93,9 @@ void adaugaMembruInArbore(Nod** rad, Membru mNou) {
 	}
 
 	int dif = calculDiferentaInaltimi(*rad);
+	if (dif == 2) {
+		if (calculDiferentaInaltimi((*rad)->st) == -1)
+			rotireLaStanga(&(*rad)->st);
+
+		rotireLaDreapta(rad);
+	}
