c2a56cf18774bc9d08e0b134bc8a49e547087eea
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-16 18:40:39 +0300

    Inserare AVL

--- SDDAndreea/SDDAndreea/Activitatea23.c
+++ SDDAndreea/SDDAndreea/Activitatea23.c
@@ -77,3 +77,19 @@ void rotireLaDreapta(Nod** rad) {
 	aux->dr = (*rad);
 	(*rad) = aux;
 }
+void adaugaCarteInArbore(Nod** rad, Carte cNou) {
+	if (*rad == NULL) {
+		Nod* nod = malloc(sizeof(Nod));
+		nod->info = cNou;
+		nod->st = NULL;
+		nod->dr = NULL;
+		*rad = nod;
+	}
+	else {
+		if (cNou.id < (*rad)->info.id)
+			adaugaCarteInArbore(&(*rad)->st, cNou);
+		else if (cNou.id > (*rad)->info.id)
+			adaugaCarteInArbore(&(*rad)->dr, cNou);
+	}
+
+	int dif = calculDiferentaInaltimi(*rad);
