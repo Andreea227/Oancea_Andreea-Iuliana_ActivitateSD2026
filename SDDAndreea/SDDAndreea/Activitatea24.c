a07e20e4cb2fafae5c6864fab1124a6ca74866be
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-16 18:52:30 +0300

    Inaltime arbore

--- SDDAndreea/SDDAndreea/Activitatea24.c
+++ SDDAndreea/SDDAndreea/Activitatea24.c
@@ -45,3 +45,16 @@ ObiectMarin initObiect(int id, int cap, float lungime,
 
 	return o;
 }
+int maxim(int a, int b) {
+	return (a > b ? a : b);
+}
+
+int calculeazaInaltimeArbore(Nod* rad) {
+	if (rad) {
+		return maxim(
+			calculeazaInaltimeArbore(rad->st),
+			calculeazaInaltimeArbore(rad->dr)
+		) + 1;
+	}
+	return 0;
+}
