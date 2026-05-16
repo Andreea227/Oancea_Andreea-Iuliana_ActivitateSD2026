9b2bbbb5338ac7834153f9d53642e39fe6c1f222
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-16 19:31:15 +0300

    Citire zbor din fisier

--- SDDAndreea/SDDAndreea/Activitatea27.c
+++ SDDAndreea/SDDAndreea/Activitatea27.c
@@ -19,3 +19,30 @@ struct Heap {
 	int nrElemente;
 };
 typedef struct Heap Heap;
+Zbor citireZborDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+
+	char* aux;
+	Zbor z;
+
+	aux = strtok(buffer, sep);
+	z.id = atoi(aux);
+
+	z.durata = atoi(strtok(NULL, sep));
+
+	z.intarziere = atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	z.destinatie = malloc(strlen(aux) + 1);
+	strcpy(z.destinatie, aux);
+
+	aux = strtok(NULL, sep);
+	z.companie = malloc(strlen(aux) + 1);
+	strcpy(z.companie, aux);
+
+	z.terminal = *strtok(NULL, sep);
+
+	return z;
+}
