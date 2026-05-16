a1eda9402a39cf40305113cb6b434fbbb3aaff38
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2026-05-16 18:05:18 +0300

    Citire tara din fisier

--- SDDAndreea/SDDAndreea/Activitatea21.c
+++ SDDAndreea/SDDAndreea/Activitatea21.c
@@ -19,3 +19,31 @@ struct Nod {
 	struct Nod* dr;
 };
 typedef struct Nod Nod;
+
+Tara citireTaraDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+
+	char* aux;
+	Tara t1;
+
+	aux = strtok(buffer, sep);
+	t1.id = atoi(aux);
+
+	t1.populatie = atoi(strtok(NULL, sep));
+
+	t1.pib = atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	t1.numeTara = malloc(strlen(aux) + 1);
+	strcpy_s(t1.numeTara, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	t1.capitala = malloc(strlen(aux) + 1);
+	strcpy_s(t1.capitala, strlen(aux) + 1, aux);
+
+	t1.continent = *strtok(NULL, sep);
+
+	return t1;
+}
